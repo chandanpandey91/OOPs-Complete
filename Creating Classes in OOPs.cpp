a7649319ceabae012c65fefc43251354bf3af300
Chandan Pandey <133705046+chandanpandey91@users.noreply.github.com>
2024-11-27 21:30:53 +0530

    Create Creating Classes in OOPs.cpp
    In this code file we have discussed about the creation of a class and accessing it

---
+++ Creating Classes in OOPs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class HumanBeing{
+public:
+   void display(){
+     cout<< "hello i am chandan" << endl;
+   }
+};
+
+int main(){
+  HumanBeing chandan;
+chandan.display();
+
+return 0;
+}
