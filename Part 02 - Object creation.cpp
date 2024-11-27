1b67508dd8e63753db9e6bd38be98c30dec24e25
Chandan Pandey <133705046+chandanpandey91@users.noreply.github.com>
2024-11-27 16:10:53 +0000

    object acess code added

---
+++ Part 02 - Object creation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+uisng namespace std;
+
+class HumanBeing{
+    public:
+      void display(){
+        cout << "hi i am" << name;
+      }
+};
+
+int main(){
+    HumanBeing chandan;
+    chandan.name = "chandan";
+    chandan.display();
+    return 0;
+}
